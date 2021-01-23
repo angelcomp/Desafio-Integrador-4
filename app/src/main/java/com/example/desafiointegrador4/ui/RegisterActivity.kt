7717d66ee05ab31605baafca001fb87de6fe05ea
Angelica <santosangelicassp@gmail.com>
2021-01-23 13:25:58 -0300

    Criando: Activity de cadastro / Atualizando: layout do Login

--- app/src/main/java/com/example/desafiointegrador4/ui/RegisterActivity.kt
+++ app/src/main/java/com/example/desafiointegrador4/ui/RegisterActivity.kt
@@ -1,12 +1,23 @@
 package com.example.desafiointegrador4.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.desafiointegrador4.R
+import kotlinx.android.synthetic.main.activity_register.*
 
 class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+
+        btn_create.setOnClickListener {
+            callMain()
+        }
+    }
+
+    private fun callMain() {
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
     }
 }
