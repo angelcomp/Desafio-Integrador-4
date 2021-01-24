537f3f19d9aadf69c14145ba2b51c05318835009
Angelica <santosangelicassp@gmail.com>
2021-01-24 20:20:37 -0300

    Atualizando: Login

--- app/src/main/java/com/example/desafiointegrador4/ui/LoginActivity.kt
+++ app/src/main/java/com/example/desafiointegrador4/ui/LoginActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 import android.widget.Toast
 import com.example.desafiointegrador4.R
 import com.example.desafiointegrador4.models.Usuario
@@ -16,6 +17,7 @@ class LoginActivity : AppCompatActivity() {
         setContentView(R.layout.activity_login)
 
         iv_btnLogin.setOnClickListener {
+            realizarLogin()
             callMain()
         }
 
@@ -32,5 +34,39 @@ class LoginActivity : AppCompatActivity() {
     private fun callRegister() {
         val intent = Intent(this, RegisterActivity::class.java)
         startActivity(intent)
+    }
+
+    private fun realizarLogin() {
+            val usuario = getUsuario()
+            if (usuario != null) {
+                sendFirebaseLogin(usuario)
+            } else {
+                sendMsg("Preencha o campo email e senha corretamente!")
+            }
+    }
+
+    private fun sendMsg(msg: String) {
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT)
+                .show()
+    }
+
+    private fun sendFirebaseLogin(usuario: Usuario) {
+        FirebaseAuth.getInstance().signInWithEmailAndPassword(usuario.email, usuario.senha)
+                .addOnCompleteListener {
+                    if (it.isSuccessful) {
+                        val firebaseUser = it.result?.user!!
+                        val usarFire = Usuario(firebaseUser.email.toString(), "", firebaseUser.uid)
+                        sendMsg("Login realizado com sucesso!")
+                    }
+                }
+    }
+
+    private fun getUsuario(): Usuario? {
+        var email = et_emailLogin.text.toString()
+        var senha = et_passwordLogin.text.toString()
+        return if (!email.isNullOrBlank() and !senha.isNullOrBlank())
+            Usuario("", email, senha)
+        else
+            null
     }
 }
